d7cd7553496044bdb9775f45e8650306f27ba074
asceznyk <asceznyk@gmail.com>
2021-05-28 21:51:47 +0530

    I dont think I am as bad!

--- fibonacciesum.c
+++ fibonacciesum.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
-int fibonacci_sum (int n) {
-	if(n <= 0) {
-		return 1;
-	} else {
-		return fibonacci_sum(n-1) + fibonacci_sum(n-2);
+int fibonacci_sum (int bound) {
+	int n = 1;
+	int p = 0;
+	int t = 0;	
+	while(n < bound) {
+		t = n;
+		n = n+p;	
+		p = t;
 	}
+
+	return n;
 } 
 
 int main () {
-	int sum = fibonacci_sum(5); //answer should be 13
+	int sum = fibonacci_sum(15); //answer should be 13
 	printf("%d", sum);
 	return 0;
 } 
