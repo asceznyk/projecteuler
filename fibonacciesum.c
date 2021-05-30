ff063bb6c2c88e359638ea87f07486c5920be430
asceznyk <asceznyk@gmail.com>
2021-05-30 19:33:17 +0530

    simple problem dumb solution

--- fibonacciesum.c
+++ fibonacciesum.c
@@ -3,18 +3,23 @@
 int fibonacci_sum (int bound) {
 	int n = 1;
 	int p = 0;
-	int t = 0;	
-	while(n < bound) {
+	int t = 0;
+
+	int sumeven = 0;
+	while(n < bound) { 
 		t = n;
 		n = n+p;	
 		p = t;
-	}
 
-	return n;
+		if (n % 2 == 0)
+			sumeven = sumeven + n;
+	 }
+
+	return sumeven;
 } 
 
 int main () {
-	int sum = fibonacci_sum(15); //answer should be 13
+	int sum = fibonacci_sum(4000000); //answer should be 13
 	printf("%d", sum);
 	return 0;
 } 
