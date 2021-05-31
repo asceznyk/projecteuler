b736e5587d38a3f03076888731feab211572860a
asceznyk <asceznyk@gmail.com>
2021-05-31 21:55:38 +0530

    incomplete

---
+++ largestprime.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "utils.h"
+
+/*int check_prime(int n) {
+	int f = floor();
+	return f;
+}
+
+int largest_prime(int num) {
+
+}*/
+
+int main () {
+	int n = factorial(5);
+	printf("%d", n);
+	return 0;
+}
