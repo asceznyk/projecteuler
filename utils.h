8da72ecd0caa2bb66b4d9fb2b7cdf61240d464a4
asceznyk <asceznyk@gmail.com>
2021-06-01 21:43:46 +0530

    some functions for prime

--- utils.h
+++ utils.h
@@ -1,6 +1,13 @@
+#include <math.h>
+
 int factorial (int n) {
 	if(n <= 0) 
 		return 1;
 	else
 		return n * factorial(n-1);
+}
+
+int check_prime (int n) {
+	int f = floor((factorial(n) % (n+1)) / n) * (n - 1) + 2;
+	return f;
 }
