5de789f99e707a78ad0a93ce5a31f66abbc69f53
asceznyk <asceznyk@gmail.com>
2021-06-04 21:02:19 +0530

    have to add 64bit support

--- largestprime.c
+++ largestprime.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "utils.h"
 
-/*int check_prime(int n) {
-	int f = floor();
-	return f;
+int largest_prime(int n) {
+	int x = 2;
+
+	while(n != x) {	
+		if(n % x == 0) {	
+			n = n / x;
+			printf("%d ", n);
+			x = 2;
+		} else {
+			x = x+1;
+		}
+	}
+
+	return x;
 }
 
-int largest_prime(int num) {
-
-}*/
-
 int main () {
-	int n = factorial(5);
-	printf("%d", n);
+	//long n = 600851475143;
+	int n = 13195;
+	int p = largest_prime(n);
+	printf("%d ", p);
 	return 0;
 }
